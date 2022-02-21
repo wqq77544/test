2c23cd9fb8bc2d26c55ae785d17126e68bdc0738
王强 <wqq77544@163.com>
2022-02-21 17:49:36 +0800

    in order to build my packagist

---
+++ src/Ford/Escape/Escape1.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace Ford\Escape;
+
+class Escape1
+{
+    public static function info()
+    {
+        echo "This is Ford Escape2016!<br />";
+    }
+}
---
+++ src/Ford/Fusion/Fusion1.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace Ford\Fusion;
+
+class Fusion1
+{
+    public static function info()
+    {
+        echo "This is Ford Fusion1!<br />";
+    }
+}
